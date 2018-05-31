b2e3bed49af2ef3a736b6a5f8c933843d8cf8d67
dcDalin <mcdalinoluoch@gmail.com>
2018-05-31 13:52:43 +0300

    :arrow_upper_right: [Chore #157931208] Change API run

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 #! run.py
 # -*- coding: utf-8 -*-
 """Entry point of the app
-
 Runs the app in specified exported mode
 """
 
 # import the APP used to instanciate flask
+from api.server import APP
 
-from api.server import main
-
-if __name__ == '__main__':
-    main()
+# run the app
+APP.run()
