f1945ec31507854f628f2694cd23f650cfacf855
dcDalin <mcdalinoluoch@gmail.com>
2018-06-02 18:45:18 +0300

    :bug: [Bug] Fix host path for heroku application run

--- run.py
+++ run.py
@@ -3,10 +3,14 @@
 """Entry point of the app
 Runs the app in specified exported mode
 """
+import os
 
 # import the APP used to instanciate flask
 from api.server import APP
 
 # run the app
-# APP.run()
-APP.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    APP.debug = True
+    port = int(os.environ.get('PORT', 5000))
+    APP.run(host='0.0.0.0', port=port)
