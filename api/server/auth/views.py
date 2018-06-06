15b2a017ab3c1efb7dfd1f8bbd9a7100a1f320d3
dcDalin <mcdalinoluoch@gmail.com>
2018-06-06 20:12:26 +0300

    :arrow_upper_right: [Chore #158138197] Set up successful response to signup route

--- api/server/auth/views.py
+++ api/server/auth/views.py
@@ -4,7 +4,7 @@
 This module contains various routes for the auth endpoint
 """
 
-from flask import Blueprint
+from flask import Blueprint, make_response, jsonify, request
 from flask.views import MethodView
 
 
@@ -15,9 +15,36 @@ AUTH_BLUEPRINT = Blueprint('auth', __name__, url_prefix='/api/v1/auth')
 class SignupAPI(MethodView):
     """User Signup resource"""
 
-    def get(self):  # pylint: disable=R0201
+    def post(self):  # pylint: disable=R0201
         """post method"""
-        return 'working', 200
+        # get the post data
+        post_data = request.get_json()
+
+        # check for no input i.e. {}
+        if not post_data:
+            response_object = {
+                'status': 'fail',
+                'msg': 'No input data provided.'
+            }
+            return make_response(jsonify(response_object)), 400
+        # Store user input in variables
+        input_first_name = post_data.get('first_name')
+        input_last_name = post_data.get('last_name')
+        input_email = post_data.get('email')
+        input_password = post_data.get('password')
+        # return response
+        response_object = {
+            "status": 'success',
+            "msg": "Account for '{}' has been created.".format(
+                input_email),
+            "user": {
+                "first_name": input_first_name,
+                "last_name": input_last_name,
+                "email": input_email
+            }
+
+        }
+        return make_response(jsonify(response_object)), 201
 
 
 # define API resources
@@ -28,5 +55,5 @@ SIGNUP_VIEW = SignupAPI.as_view('signup_api')
 AUTH_BLUEPRINT.add_url_rule(
     '/signup',
     view_func=SIGNUP_VIEW,
-    methods=['GET']
+    methods=['POST']
 )
