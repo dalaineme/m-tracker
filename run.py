09512f495419ea475eb470cc3f3c45165a658087
dcDalin <mcdalinoluoch@gmail.com>
2018-06-10 23:53:47 +0300

    Heroku config

--- run.py
+++ run.py
@@ -10,5 +10,6 @@ from api.server import APP
 # run the app
 
 if __name__ == '__main__':
-    # PORT = int(os.environ.get('PORT', 5000))
-    APP.run()
+    APP.debug = True
+    port = int(os.environ.get('PORT', 5000))
+    APP.run(host='0.0.0.0', port=port)
